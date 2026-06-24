26ece59bd17a9aaf5931f643274f02c34e3cef15
Willson <yikai.zhu@yahoo.com>
2026-06-23 19:30:59 -0500

    Updated main file name

---
+++ src/CTA_speedrun.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class CTA_speedrun {
+
+    public static void main(){
+
+        System.out.println("test");
+    }
+
+}
