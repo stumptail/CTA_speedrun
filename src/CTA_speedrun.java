2adc6b80899be36ed2232a2513a2d97643dc158e
Willson <yikai.zhu@yahoo.com>
2026-06-24 22:47:11 -0500

    Working on regex

--- src/CTA_speedrun.java
+++ src/CTA_speedrun.java
@@ -1,10 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CTA_speedrun {
 
     public static void main(){
+        String regex = "(\\d\\d):(\\d\\d):(\\d\\d),(\\d\\d\\d) --> (\\d\\d):(\\d\\d):(\\d\\d),(\\d\\d\\d)";
+        Pattern pattern = Pattern.compile(regex);
+    }
 
-        System.out.println("test");
+    public static void detection (){
+
     }
 
 }
